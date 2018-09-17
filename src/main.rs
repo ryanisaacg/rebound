c102fa02aa463045c7b5acb14e2a69a28bd7fd94
Ryan Goldstein <ryanisaacg@gmail.com>
2018-09-17 10:12:03 -0400

    Add an entity msotly to check the unique store is vaguely working

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ extern crate ncollide2d;
 extern crate quicksilver;
 
 use ncollide2d::{
+    event::ContactEvent,
     shape::{Cuboid, Shape, ShapeHandle},
     world::{CollisionGroups, CollisionObjectHandle, CollisionWorld, GeometricQueryType}
 };
 use quicksilver::{
     *,
-    geom::*,
+    geom::{*, Shape as ShapeTrait},
     graphics::*,
     input::*,
     lifecycle::*,
@@ -16,12 +17,12 @@ use quicksilver::{
 };
 
 mod unique_store;
-use unique_store::{Key, KeyAllocator, UniqueStore};
+use unique_store::{Key, KeyAllocator, UniqueStore, from};
 
-const PIXELS_PER_UNIT: f32 = 32.0;
+const PIXELS_PER_UNIT: f32 = 100.0;
 const PLAYER_SIZE: Vector = Vector {
-    x: 1.0,
-    y: 1.0,
+    x: 0.16,
+    y: 0.16,
 };
 
 #[derive(Copy, Clone)]
@@ -30,10 +31,18 @@ enum CollisionProp {
     Entity(Key),
 }
 
+#[derive(Copy, Clone)]
+enum EntityType {
+    Player
+}
+
+use EntityType::*;
+
 struct Store {
     world: CollisionWorld<f32, CollisionProp>,
     key_alloc: KeyAllocator,
     bounds: UniqueStore<CollisionObjectHandle>,
+    types: UniqueStore<EntityType>,
     player: Key,
 }
 
@@ -43,13 +52,15 @@ impl Store {
             world: CollisionWorld::new(0.02),
             key_alloc: KeyAllocator::new(),
             bounds: UniqueStore::new(),
+            types: UniqueStore::new(),
             player: Key::null(),
         };
-        state.player = state.add((0, 0), 0, Cuboid::new((PLAYER_SIZE / 2).into_vector()), true);
+        state.player = state.add((0, 0), 0, Cuboid::new((PLAYER_SIZE / 2).into_vector()), true, Player);
+        state.player = state.add((0, 0), 0, Cuboid::new((PLAYER_SIZE / 2).into_vector()), true, Player);
         state
     }
 
-    fn add(&mut self, pos: impl Into<Vector>, angle: impl Scalar, bounds: impl Shape<f32>, solid: bool) -> Key {
+    fn add(&mut self, pos: impl Into<Vector>, angle: impl Scalar, bounds: impl Shape<f32>, solid: bool, entity_type: EntityType) -> Key {
         let key = self.key_alloc.alloc();
         let pos: Vector = pos.into();
         let bounds = ShapeHandle::new(bounds);
@@ -59,6 +70,7 @@ impl Store {
         };
         let isometry = na::Isometry2::new(pos.into_vector(), angle.float());
         self.bounds.insert(key, self.world.add(isometry, bounds, CollisionGroups::new(), query_type, CollisionProp::Entity(key)));
+        self.types.insert(key, entity_type);
         key
     }
 }
@@ -75,13 +87,28 @@ impl State for Game {
     }
 
     fn update(&mut self, window: &mut Window) -> Result<()> {
+        let world = &mut self.store.world;
+        self.store.bounds.iter().for_each(|(_, bounds)| {
+            let object = world.collision_object_mut(*bounds).unwrap();
+            let position: Vector = object.position().translation.vector.into();
+            let transform = na::Isometry2::new((position + Vector::new(0.01, 0.01)).into_vector(), 0.0);
+            object.set_position(transform);
+        });
         Ok(())
     }
 
     fn draw(&mut self, window: &mut Window) -> Result<()> {
         use Background::*;
         window.clear(Color::BLACK)?;
-        window.draw(&Rectangle::new((50, 50), (50, 50)), Col(Color::WHITE));
+        from(self.store.bounds.iter(), self.store.types.iter()).for_each(|(_, (bounds, ent_type))| {
+            let color = match ent_type {
+                Player => Color::BLUE,
+            };
+            let object = self.store.world.collision_object(*bounds).unwrap();
+            let position: Vector = object.position().translation.vector.into();
+            let area = Rectangle::new_sized((32, 32)).translate(position * PIXELS_PER_UNIT);
+            window.draw(&area, Col(color));
+        });
         Ok(())
     }
 }
